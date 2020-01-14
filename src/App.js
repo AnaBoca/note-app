a2a645468010f578f2e56a3aa0c6d4401898da62
Ana Boca <Ana.Boca@asu.edu>
2020-01-14 23:37:14 +0000

    showNote state

--- src/App.js
+++ src/App.js
@@ -1,17 +1,24 @@
 import React from 'react';
-import logo from './logo.svg';
 import './App.css';
 import Nav from './components/Nav';
 import List from './components/List';
 import Note from './components/Note';
 
 class App extends React.Component {
+  constructor() {
+    super();
+    this.state = {
+      showNote: false
+    };
+  }
+
   render() {
+    const { showNote } = this.state;
+
     return (
       <div className="App">
         <Nav />
-        <List />
-        <Note />
+        { showNote ? <Note /> : <List /> }
       </div>
     );
   }
